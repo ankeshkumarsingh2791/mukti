784c0fe964a5e2852b239b00306abd75470a24ff
ankeshkumarsingh2791 <ankeshkumarsingh2791@gmail.com>
2024-11-29 00:18:33 -0800

    backend routing and env file

--- server/back.js
+++ server/back.js
@@ -1,3 +1,33 @@
-export default  backend = () => {
-    console.log("Hello Server")
-} 
+const express = require('express')
+const colors = require('colors')
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+
+// dotenv config
+
+dotenv.config();
+
+// rest object
+const app = express()
+
+// middlewares
+
+app.use(express.json())
+app.use(morgan('dev'))
+
+//routes
+
+app.get('/', (req,res) => {
+    res.status(200).send({
+        message:"server is running "
+    })
+});
+
+// listen port
+ const port = process.env.PORT || 8080
+
+ app.listen(port, () =>{
+    console.log(
+        `server is running in ${process.env.DEV_MODE} mode on port ${process.env.PORT}`.bgCyan.white
+    );
+ });
